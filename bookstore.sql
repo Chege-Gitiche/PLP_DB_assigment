584cd13f41ce57b81d74025962a005d5914a033c
Chege-Gitiche <104974375+Chege-Gitiche@users.noreply.github.com>
2025-04-13 22:54:02 +0300

    Update bookstore.sql

--- bookstore.sql
+++ bookstore.sql
@@ -3,27 +3,6 @@ USE bookstoredb;
 
 
 Question 1 
-CREATE TABLE author (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    birth_year INT,
-    nationality VARCHAR(100)
-);
-
-Question 2
-CREATE TABLE book_language (
-    language_id INT AUTO_INCREMENT PRIMARY KEY,
-    language_name VARCHAR(100) NOT NULL
-);
-
-Question 3
-CREATE TABLE publisher (
-    publisher_id INT AUTO_INCREMENT PRIMARY KEY,
-    publisher_name VARCHAR(255) NOT NULL,
-    country VARCHAR(100)
-);
-
-Question 4
 CREATE TABLE book (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
@@ -35,7 +14,8 @@ CREATE TABLE book (
     FOREIGN KEY (language_id) REFERENCES book_language(language_id)
 );
 
-Question 5
+
+Question 2
 CREATE TABLE book_author (
     book_id INT,
     author_id INT,
@@ -43,6 +23,30 @@ CREATE TABLE book_author (
     FOREIGN KEY (book_id) REFERENCES book(book_id),
     FOREIGN KEY (author_id) REFERENCES author(author_id)
 ); 
+
+
+Question 3
+CREATE TABLE author (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    birth_year INT,
+    nationality VARCHAR(100)
+);
+
+
+Question 4
+CREATE TABLE book_language (
+    language_id INT AUTO_INCREMENT PRIMARY KEY,
+    language_name VARCHAR(100) NOT NULL
+);
+
+Question 5
+CREATE TABLE publisher (
+    publisher_id INT AUTO_INCREMENT PRIMARY KEY,
+    publisher_name VARCHAR(255) NOT NULL,
+    country VARCHAR(100)
+);
+
 
 
 Question 6
